a27bf81bf669f9f3d29f7d28ff2bc4529b209574
alexsomeoddpilot <alex@someoddpilot.com>
2014-11-03 15:47:38 -0600

    Further testing

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ handlebars.registerPartial('header', '<header/>');
 
 it('should compile Handlebars templates', function (cb) {
 
-  var stream = template(handlebars);
+  var stream = template({handlebars: handlebars});
 
   stream.on('data', function (data) {
     assert.equal(data.contents.toString(), "<header/><li>Lorem</li>\n<li>Ipsum</li>\n");
@@ -20,6 +20,43 @@ it('should compile Handlebars templates', function (cb) {
       "people": ["Lorem", "Ipsum"]
     }))
   }));
+
+  stream.end();
+});
+
+it('should throw errors if incorrect template', function (cb) {
+
+  var stream = template(handlebars);
+
+  try {
+    stream.write(new gutil.File({
+      contents: new Buffer(JSON.stringify({
+        "template": "notreal.handlebars",
+        "people": ["Lorem", "Ipsum"]
+      }))
+    }));
+  } catch (error) {
+    assert.equal(error.message, "Template does not exist");
+    cb();
+  }
+
+  stream.end();
+});
+
+it('should throw errors if missing template', function (cb) {
+
+  var stream = template(handlebars);
+
+  try {
+    stream.write(new gutil.File({
+      contents: new Buffer(JSON.stringify({
+        "people": ["Lorem", "Ipsum"]
+      }))
+    }));
+  } catch (error) {
+    assert.equal(error.message, "Missing template parameter");
+    cb();
+  }
 
   stream.end();
 });
